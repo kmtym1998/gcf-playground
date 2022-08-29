6aea08f4a7258c2af76951c803aeadc6804da96c
kmtym1998 <kmtym.devloper@gmail.com>
2022-08-29 15:23:04 +0900

    IP 見る

--- go-read-file/function.go
+++ go-read-file/function.go
@@ -3,10 +3,12 @@ package f
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
@@ -16,6 +18,25 @@ func init() {
 }
 
 func ListFiles(w http.ResponseWriter, r *http.Request) {
+	// IP 見る
+	func() {
+		url := "https://rakko.tools/tools/2/"
+		byteResp, httpResp, err := Request(http.MethodGet, url, nil, nil)
+		if err != nil {
+			log.Fatalln("err httpwrap.Request", err)
+		}
+
+		htmlResp := string(byteResp)
+		startTag := `<textarea id="resultTextArea">`
+		endTag := `</textarea>`
+		startTagPosition := strings.Index(htmlResp, startTag)
+		endTagPosition := strings.LastIndex(htmlResp, endTag) // </textarea> が 2つあるのでその後の方
+
+		println(htmlResp[startTagPosition+len(startTag) : endTagPosition])
+
+		println("httpResp.Status", httpResp.Status)
+	}()
+
 	// 静的ファイルを読み取りたい
 	func() {
 		b, err := os.ReadFile("static/hoge.json")
@@ -89,4 +110,40 @@ func ListFiles(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Print(string(logEntry))
 	}()
+}
+
+type reqHeaders struct {
+	Key   string
+	Value string
+}
+
+func Request(method string, url string, body *[]byte, headers []reqHeaders) ([]byte, *http.Response, error) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for _, rh := range headers {
+		req.Header.Set(rh.Key, rh.Value)
+	}
+
+	client := new(http.Client)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Printf("err resp.Body.Close(). err: %+v, resp: %+v", err, resp)
+		}
+	}()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return b, resp, err
 }
