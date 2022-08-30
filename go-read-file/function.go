6bf862a3f56fbcf25f44b04365c1e021d1f8589d
kmtym1998 <kmtym.devloper@gmail.com>
2022-08-30 14:24:42 +0900

    rename function

--- go-read-file/function.go
+++ go-read-file/function.go
@@ -1,13 +1,9 @@
 package f
 
 import (
-	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
@@ -15,10 +11,10 @@ import (
 )
 
 func init() {
-	functions.HTTP("ListFiles", ListFiles)
+	functions.HTTP("Ping", Ping)
 }
 
-func ListFiles(w http.ResponseWriter, r *http.Request) {
+func Ping(w http.ResponseWriter, r *http.Request) {
 	func() {
 		uri := "user=kmtym1998 password=kmtym1998 database=app host=10.0.0.2 port=5432"
 		pg := postgres.NewPGService(uri)
@@ -49,80 +45,6 @@ func ListFiles(w http.ResponseWriter, r *http.Request) {
 		println(htmlResp[startTagPosition+len(startTag) : endTagPosition])
 
 		println("httpResp.Status", httpResp.Status)
-	}()
-
-	// 静的ファイルを読み取りたい
-	func() {
-		b, err := os.ReadFile("static/hoge.json")
-		if err != nil {
-			log.Println(err)
-		}
-		log.Println("static json", string(b))
-
-		b, err = os.ReadFile(os.Getenv("SOURCE_DIR") + "static/hoge.json")
-		if err != nil {
-			log.Println(err)
-		}
-
-		log.Println("static json", string(b))
-	}()
-
-	// カレントディレクトリ
-	func() {
-		currentDir, _ := os.Getwd()
-		log.Println("currentDir", currentDir)
-	}()
-
-	// /workspace 配下のファイルを読む
-	func() {
-		err := filepath.Walk("/workspace", func(path string, _ os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-
-			fmt.Printf("path: %#v\n", path)
-			return nil
-		})
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	// /workspace/main.go 配下のファイルを読む
-	func() {
-		b, err := os.ReadFile("/workspace/main.go")
-		if err != nil {
-			panic(err)
-		}
-		log.Println("== main.go ==========")
-		log.Println(string(b))
-		log.Println("============")
-
-		b, err = os.ReadFile("/workspace/go.mod")
-		if err != nil {
-			panic(err)
-		}
-		log.Println("== go.mod ==========")
-		log.Println(string(b))
-		log.Println("============")
-
-		dirs, err := os.ReadDir("/")
-		if err != nil {
-			panic(err)
-		}
-
-		for _, dir := range dirs {
-			log.Println("dir.Name()", dir.Name())
-		}
-		logEntry, err := json.Marshal(map[string]string{
-			"severity": "DEBUG",
-			"message":  string(b),
-		})
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Print(string(logEntry))
 	}()
 }
 
